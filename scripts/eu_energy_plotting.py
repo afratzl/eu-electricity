ce64823845455a4e3b1df851758951265ceb13be
Alex Fratzl <32060048+afratzl@users.noreply.github.com>
2025-11-24 23:30:07 +0100

    Update eu_energy_plotting.py

--- scripts/eu_energy_plotting.py
+++ scripts/eu_energy_plotting.py
@@ -838,7 +838,6 @@ def create_all_charts(all_data):
 
             fig.suptitle('Renewables vs Non-Renewables',
                          fontsize=34, fontweight='bold', x=0.5, y=0.995, ha="center")
-                         fontsize=30, fontweight='bold', x=0.5, y=0.995, ha="center")
 
             plt.tight_layout(rect=[0, 0.02, 1, 0.985])
 
